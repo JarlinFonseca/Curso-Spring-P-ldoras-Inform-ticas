82f0829049129cff084ad71df6eb5deb4941f59b
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-02-07 17:31:30 -0500

    Inyección de Dependencias usando el Constructor

--- src/es/pildoras/IoC/Empleados.java
+++ src/es/pildoras/IoC/Empleados.java
@@ -3,5 +3,7 @@ package es.pildoras.IoC;
 public interface Empleados {
 	
 	public String getTareas();
+	
+	public String getInforme();
 
 }
--- src/es/pildoras/IoC/JefeEmpleado.java
+++ src/es/pildoras/IoC/JefeEmpleado.java
@@ -1,10 +1,27 @@
 package es.pildoras.IoC;
 
 public class JefeEmpleado implements Empleados {
+	//Creación de campo tipo creaciónInforme(interfaz)
 	
+	private CreacionInformes informeNuevo;
+		
+	//Creación de constructor que inyecta la dependencia
+	
+	public JefeEmpleado(CreacionInformes informeNuevo) {
+		this.informeNuevo = informeNuevo;
+	}
+
+		
 	@Override
 	public String getTareas() {
 		return "Gestiono las cuestiones relativas a mis empleados de sección";
+	}
+
+
+	@Override
+	public String getInforme() {
+		// TODO Auto-generated method stub
+		return "Informe presentado por el Jefe con arreglos: "+informeNuevo.getInforme();
 	}
 
 }
