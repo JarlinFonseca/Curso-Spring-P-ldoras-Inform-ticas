114336d63c875f8b1834e05bae297c2e49767401
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-02-09 16:37:33 -0500

    Implementar las interfaces InitializingBean y DisposableBean, y crer el metodo afterPropertiesSet() que se ejecuta despues de que se crea el bean (remplaza @PostConstruct ) y el metodo destroy() que se ejecuta antes de la destruccion del bean (remplaza @PreDestroy )

--- SpringConAnnotations/src/es/pildoras/pruebaannotations/ComercialExperimentado.java
+++ SpringConAnnotations/src/es/pildoras/pruebaannotations/ComercialExperimentado.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.DisposableBean;
 import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Scope;
+//import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
