18b1e96b9a12d78c635aeefb5fd9387f53c5e0cb
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-02-09 15:47:03 -0500

    Usando @Scope para definir los patrones Singleton o Prototype con Java annotations. Esto simplifica mucho el preoceso respecto a hacerlo con el xml de configuración.

--- SpringConAnnotations/src/es/pildoras/pruebaannotations/ComercialExperimentado.java
+++ SpringConAnnotations/src/es/pildoras/pruebaannotations/ComercialExperimentado.java
@@ -2,9 +2,11 @@ package es.pildoras.pruebaannotations;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope("prototype")
 public class ComercialExperimentado implements Empleados {
 	
 	@Autowired
---
+++ SpringConAnnotations/src/es/pildoras/pruebaannotations/UsoAnnotation2.java
@@ -0,0 +1,33 @@
+package es.pildoras.pruebaannotations;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class UsoAnnotation2 {
+
+	public static void main(String[] args) {
+		
+		// Leer el XML de configuración
+		ClassPathXmlApplicationContext contexto = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// Pedir un Bean al contenedor
+		Empleados Antonio = contexto.getBean("comercialExperimentado", Empleados.class);
+		
+		Empleados Lucia = contexto.getBean("comercialExperimentado", Empleados.class);
+		
+		// Usar el Bean
+		
+		// ¿Apuntan al mismo objeto en memoria?
+		
+		if(Antonio.equals(Lucia)) { 
+			System.out.println("Apuntan al mismo lugar en memoria");
+			System.out.println(Antonio+"\n"+Lucia);
+		}else {
+			System.out.println("No apuntan al mismo lugar en memoria");
+			System.out.println(Antonio+"\n"+Lucia);
+		}
+		// Cerrar el contexto
+		contexto.close();
+
+	}
+
+}
