f3a84a42f14d97920f95ca3c8854305dc96fb4f0
Jarlin Andres Fonseca Bermon <jarlinandres5000@gmail.com>
2023-02-09 16:30:01 -0500

    Implementar las interfaces InitializingBean y DisposableBean, y crer el metodo afterPropertiesSet() que se ejecuta despues de que se crea el bean (remplaza @PostConstruct ) y el metodo destroy() que se ejecuta antes de la destruccion del bean (remplaza @PreDestroy )

--- SpringConAnnotations/src/es/pildoras/pruebaannotations/ComercialExperimentado.java
+++ SpringConAnnotations/src/es/pildoras/pruebaannotations/ComercialExperimentado.java
@@ -1,13 +1,15 @@
 package es.pildoras.pruebaannotations;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
-@Scope("prototype")
-public class ComercialExperimentado implements Empleados {
+//@Scope("prototype")
+public class ComercialExperimentado implements Empleados, InitializingBean, DisposableBean  {
 	
 	@Autowired
 	@Qualifier("informeFinancieroTrim4") // been ID que debe utilizar
@@ -29,6 +31,22 @@ public class ComercialExperimentado implements Empleados {
 		 * @Autowired public void q(CreacionInformeFinanciero creacionInformeFinanciero)
 		 * { this.creacionInformeFinanciero = creacionInformeFinanciero; }
 		 */
+	  
+	  // No sirve con el patrón de diseño Prototype(Spring no maneja el ciclo completo de un Bean),
+	  //solo sirve con Singleton.
+	  
+	  // Ejecución de código después de creación del Bean
+	  // Se ejecuta despues de que se crea el bean (remplaza @PostConstruct )
+	  public void afterPropertiesSet()  {
+		  System.out.println("Ejecutado tras creación de Bean");
+	  }
+	  
+	  // Ejecuación de código después de apagado contenedor Spring
+	  // Se ejecuta antes de la destruccion del bean (remplaza @PreDestroy )
+	  public void destroy() {
+		  System.out.println("Ejecutando antes de la destrucción");
+	  }
+	  
 
 
 	@Override
